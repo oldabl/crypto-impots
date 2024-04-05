28c6e49aee542e7dccd4caedd9c2be525da9dc9c
Olivier B <obrunel@proton.me>
2024-04-05 14:32:32 +0200

    Add two test files with possibility to run locally

--- tests/test_parse_line.py
+++ tests/test_parse_line.py
@@ -1,10 +1,10 @@
-# test_parse_statements.py
+# test_parse_line.py
 
 import sys, pytest
 if __name__ == '__main__':
   sys.path.append('src')
 
-from StatementHandler import StatementHandler
+from StatementLine import StatementLine
 
 def test_capital_case():
   assert 1+1 == 2
