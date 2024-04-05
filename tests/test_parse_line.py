b338b3a4fef358399b8cd273761978bbc4360be0
Olivier B <obrunel@proton.me>
2024-04-05 15:26:20 +0200

    Add proper test for Coinbase statement line

--- tests/test_parse_line.py
+++ tests/test_parse_line.py
@@ -1,11 +1,43 @@
 # test_parse_line.py
 
-import sys, pytest
+import sys, pytest, datetime
+
+# FOR LOCAL RUN
 if __name__ == '__main__':
   sys.path.append('src')
+# FOR LOCAL RUN
+
 
 from StatementLine import StatementLine
 
-def test_capital_case():
-  assert 1+1 == 2
+def test_parse_coinbase_line():
+  timestamp = "2024-03-26 23:12:03 UTC"
+  trans_type = "Sell"
+  asset = "FIDA"
+  quantity_trans = "46.165532"
+  spot_price_curr = "EUR"
+  spot_price_at_trans = "0.48"
+  subtotal = "22.38"
+  total_with_fees = "20.89"
+  fees = "-1.49"
+  notes = "Sold 46.165532 FIDA for 20.89 EUR"
+  coinbase_line = ",".join([timestamp, trans_type, asset, quantity_trans, spot_price_curr, spot_price_at_trans, subtotal, total_with_fees, fees, notes])
 
+  stl = StatementLine(coinbase_line)
+
+  assert stl.getDate() == datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S %Z")
+  assert stl.getOpType() == trans_type
+  assert stl.getCrypto() == asset
+  assert stl.getQuantity() == float(quantity_trans)
+  assert stl.getSpotCurrency() == spot_price_curr
+  assert stl.getSpotPrice() == float(spot_price_at_trans)
+  assert stl.getSubTotal() == float(subtotal)
+  assert stl.getTotalWFees() == float(total_with_fees)
+  assert stl.getFees() == float(fees)
+  assert stl.getDesc() == notes
+
+
+# FOR LOCAL RUN
+if __name__ == '__main__':
+  test_parse_coinbase_line()
+# FOR LOCAL RUN
