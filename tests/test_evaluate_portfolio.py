05bde1a79c41f087babe3a9db40022d26066c3ff
Olivier B <obrunel@proton.me>
2024-04-07 01:43:41 +0200

    Use pytest mock got non working requests

--- tests/test_evaluate_portfolio.py
+++ tests/test_evaluate_portfolio.py
@@ -13,8 +13,9 @@ def makeFullPath(path):
 from StatementHandler import StatementHandler
 from PortfolioHandler import PortfolioHandler
 
-def test_evaluate_taxable_gains(mocker):
-  result = {2023: -0.8665641249477958, 2024: 947.8437781903135}
+result = {2023: -0.8665641249477958, 2024: 947.8437781903135}
+
+def test_evaluate_taxable_gains_no_year(mocker):
   mocker.patch('PortfolioHandler.PortfolioHandler.getTaxableGainsPerYear', return_value=result)
   
   sth = StatementHandler(makeFullPath('test_files/statements/portfolio_evaluation/portfoliostatement.csv'))
@@ -26,12 +27,60 @@ def test_evaluate_taxable_gains(mocker):
 
   assert portfolio.getTaxableGainsPerYear() == result
   assert portfolio.getTaxableGainsPerYear("") == result
+
+
+def test_evaluate_taxable_gains_error_year(mocker):
+  mocker.patch('PortfolioHandler.PortfolioHandler.getTaxableGainsPerYear', return_value=result)
+  
+  sth = StatementHandler(makeFullPath('test_files/statements/portfolio_evaluation/portfoliostatement.csv'))
+  sth.uniqueLines()
+  sth.sortDateAscending()
+  
+  portfolio = PortfolioHandler(sth)
+  #portfolio.examinePortfolioForTaxableGains()
+
+  assert portfolio.getTaxableGainsPerYear(10000000) == result
+  assert portfolio.getTaxableGainsPerYear("HJBJK7") == result
+
+
+def test_evaluate_taxable_gains_year_full_string_int(mocker):
+  mocker.patch('PortfolioHandler.PortfolioHandler.getTaxableGainsPerYear', return_value=result)
+  
+  sth = StatementHandler(makeFullPath('test_files/statements/portfolio_evaluation/portfoliostatement.csv'))
+  sth.uniqueLines()
+  sth.sortDateAscending()
+  
+  portfolio = PortfolioHandler(sth)
+  #portfolio.examinePortfolioForTaxableGains()
+
   assert portfolio.getTaxableGainsPerYear("2023") == result[2023]
   assert portfolio.getTaxableGainsPerYear(2023) == result[2023]
-  assert portfolio.getTaxableGainsPerYear(10000000) == result
+
+
+def test_evaluate_taxable_gains_year_full(mocker):
+  mocker.patch('PortfolioHandler.PortfolioHandler.getTaxableGainsPerYear', return_value=result)
+  
+  sth = StatementHandler(makeFullPath('test_files/statements/portfolio_evaluation/portfoliostatement.csv'))
+  sth.uniqueLines()
+  sth.sortDateAscending()
+  
+  portfolio = PortfolioHandler(sth)
+  #portfolio.examinePortfolioForTaxableGains()
+
   assert portfolio.getTaxableGainsPerYear(2024) == result[2024]
+
+
+def test_evaluate_taxable_gains_year_empty(mocker):
+  mocker.patch('PortfolioHandler.PortfolioHandler.getTaxableGainsPerYear', return_value=0)
+  
+  sth = StatementHandler(makeFullPath('test_files/statements/portfolio_evaluation/portfoliostatement.csv'))
+  sth.uniqueLines()
+  sth.sortDateAscending()
+  
+  portfolio = PortfolioHandler(sth)
+  #portfolio.examinePortfolioForTaxableGains()
+
   assert portfolio.getTaxableGainsPerYear(2022) == 0
-  assert portfolio.getTaxableGainsPerYear("HJBJK7") == result
 
 
 # FOR LOCAL RUN
