7efb7f05e92267288d8c3b48fc222f8929cb711b
Olivier Brunel <obrunel@proton.me>
2024-04-10 13:58:22 +0200

    Remove milliseconds from date format in logger

--- src/Defaults.py
+++ src/Defaults.py
@@ -6,6 +6,8 @@ class Defaults:
   CURRENCY = "EUR"
   COMPATIBLE_PLATFORMS = ["Coinbase", "Kraken"]
 
-logging.basicConfig(stream=open("debug.log", "w"),
+logging.basicConfig(filename="debug.log",
+                    filemode="w",
+                    datefmt="%Y-%m-%d %H:%M:%S",
                     level=logging.DEBUG,
                     format='%(asctime)s:%(levelname)s:%(module)s:%(funcName)s: %(message)s')
